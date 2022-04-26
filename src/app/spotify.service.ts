b5342a685221dea0564effa1e6f509d637116097
Loui Andrae Palo <louiandraepalo@gmail.com>
2022-04-26 22:51:27 +0200

    :wrench: domain

--- src/app/spotify.service.ts
+++ src/app/spotify.service.ts
@@ -1,19 +1,22 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../environments/environment";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class SpotifyService {
-  constructor(private http: HttpClient) {}
+  domain: string = environment.domain;
+
+  constructor(private http: HttpClient) { }
 
   searchTrack(query: string) {
-    const url = `http://localhost:5000/api/search_track?q=${query}&type=track`;
+    const url = `http://${this.domain}/api/search_track?q=${query}&type=track`;
     return this.http.get(url);
   }
 
   getTrack(id: string) {
-    const url = `http://localhost:5000/api/get_track?id=${id}`;
+    const url = `http://${this.domain}/api/get_track?id=${id}`;
     return this.http.get(url);
   }
 }
